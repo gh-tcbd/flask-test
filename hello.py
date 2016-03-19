8029c5cdff5b2d0f05b4cd7c32c491a3b34dcb75
scensorECHO <xxartofwarxx@gmail.com>
2016-03-19 17:06:16 -0400

    Updated to allow parameterized requests

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/Brandon")
+def helloBrandon():
+    return "Hello Brandon!"
+
+@app.route("/hello")
+def helloTo():
+    name = request.args.get('name', '')
+    return "Hello {}".format(name)
+    #return "Hello person!"
 
 if __name__ == "__main__":
     app.run()
